ff8e962ecd4e842751054db42e0a0e212343ed5c
José Ronquillo <114964339+JoseRonquillo@users.noreply.github.com>
2023-05-01 22:36:03 -0600

    main del programa

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+  num1 = int(input("Ingrese el primer numero"))
+  num2 = int(input("Ingrese el segundo numero"))
+  res = resultado(num1,num2)
+  print("La suma es:",res)
+  
+main()
